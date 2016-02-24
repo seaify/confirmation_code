9d86092e8712467261167a1f856228690b2d297d
seaify <dilin.life@gmail.com>
2016-02-24 16:18:00 +0800

    update

--- run.rb
+++ run.rb
@@ -5,8 +5,8 @@ require 'confirmation_code'
 
 ConfirmationCode.use :lianzhong, 'seaify', '67c86225'
 ap ConfirmationCode.account
-#result = ConfirmationCode.upload 'https://passport.58.com/validatecode?temp=123i1knr04o'
-#ap result
-#ap ConfirmationCode.recognition_error result['data']['id']
+result = ConfirmationCode.upload 'https://passport.58.com/validatecode?temp=123i1knr04o'
+ap result
+ap ConfirmationCode.recognition_error result['data']['id']
 ap ConfirmationCode.account
 
