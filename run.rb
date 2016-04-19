1d39627c0f4413e7ff3fb6b5123e417bf7ac4bd4
seaify <dilin.life@gmail.com>
2016-04-19 14:02:41 +0800

    remove activesupport dependency

--- run.rb
+++ run.rb
@@ -10,7 +10,6 @@ ap ConfirmationCode.account
 result = ConfirmationCode.upload('http://captcha.qq.com/getimage')
 ap result
 ap ConfirmationCode.recognition_error result['data']['id']
-return
 
 ConfirmationCode.use :damatu, 'seaify', 'lsm123', {app_key: '0f80784a5ff20d38df3977e461e3d82a', app_id: '41635'}
 ap ConfirmationCode.account
